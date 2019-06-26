75b485eda724a76d16e028447f57c9a85f469878
Skiednia <agatha.floors@gmail.com>
2019-06-26 19:47:07 -0300

    adc novo item

--- m3/aula/4/todo.js
+++ m3/aula/4/todo.js
@@ -12,6 +12,8 @@ var todos = [
 ];
 
   function renderTodos() {
+       listElement.innerHTML = '';
+
       for(todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -20,13 +22,14 @@ var todos = [
         listElement.appendChild(todoElement);
       }
   }
+      renderTodos();
 
-  renderTodos();
-
-  function adcTodo() {
+  function addTodo() {
     var todoText = inputElement.value;
 
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
   }
+
+  buttonElement.onclick = addTodo
