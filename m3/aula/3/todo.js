e4a4bd8e1dbb90a43cc36db0a99fea3689592e89
Skiednia <agatha.floors@gmail.com>
2019-06-26 19:27:19 -0300

    inicio aula 3

---
+++ m3/aula/3/todo.js
@@ -0,0 +1,18 @@
+var listElement = 
+document.querySelector("#app ul");
+var inputElement = 
+document.querySelector("#app input");
+var buttonElement = 
+document.querySelector("#app button");
+
+var todos = [
+    'fazer café',
+    'beber gasolina',
+    'jogar o canudo no lixo'
+];
+
+  function renderTodos() {
+      for(todo of todos) {
+          
+      }
+  }
