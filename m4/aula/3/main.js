b6228310055466ee0a7a3a896188fb12502e65ac
Skiednia <agatha.floors@gmail.com>
2019-06-27 15:55:13 -0300

    fim aula 3

--- m4/aula/3/main.js
+++ m4/aula/3/main.js
@@ -1,21 +1,9 @@
-var aPromise = function() {
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/skiednia');
-        xhr.send(null);
+axios.get('https://api.github.com/users/skiednia')
+  .then(function(response) {
+      console.log(response)
+  })
+    
+  .catch(function(error) {
+      console.warn(error)
+  });
 
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-aPromise()
-  .then(function(response) {})
-  .catch(function(error) {});
