6d5c607867a1924cc97af214f93c509c91808625
Skiednia <agatha.floors@gmail.com>
2019-06-26 21:22:18 -0300

    JSON

--- m4/aula/main.js
+++ m4/aula/main.js
@@ -1,4 +1,10 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/skiednia');
+xhr.open('GET', 'https://api.github.com/users/skiednia')
 xhr.send(null);
+
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
