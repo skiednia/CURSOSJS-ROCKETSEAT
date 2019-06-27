9ee7bc8c71ef580fcdf023838100d5140bac2844
Skiednia <agatha.floors@gmail.com>
2019-06-27 16:12:01 -0300

    inicio main

---
+++ m4/ex/1/main.js
@@ -0,0 +1,10 @@
+function checaIdade(idade) {
+    // Retornar uma promise
+   }
+   checaIdade(20)
+    .then(function() {
+    console.log("Maior que 18");
+    })
+    .catch(function() {
+    console.log("Menor que 18");
+    });
