907bbe0d63e951fafa23dcb29be7eaab1af3086d
Skiednia <agatha.floors@gmail.com>
2019-06-27 16:18:16 -0300

    resolução 1

--- m4/ex/1/main.js
+++ m4/ex/1/main.js
@@ -1,7 +1,11 @@
 function checaIdade(idade) {
-    // Retornar uma promise
+    return new Promise(function(resolve, reject){
+        setTimeout(function() {
+            return idade >= 18 ? resolve() : reject();
+        }, 2000);
+    });
    }
-   checaIdade(20)
+   checaIdade(12)
     .then(function() {
     console.log("Maior que 18");
     })
